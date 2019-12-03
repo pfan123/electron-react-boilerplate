2e218109d6614342080cb410ff3bb352a269fb23
gaolu11 <768065158@qq.com>
2019-12-03 12:24:55 +0800

    feat: 目录结构调整

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,19 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = {
+  paths: (paths, env) => {
+    paths.appBuild = path.resolve(__dirname, './dist')
+    paths.appPublic = path.resolve(__dirname, './webview/public')
+    paths.appHtml = path.resolve(__dirname, './webview/public/index.html')
+    paths.appIndexJs = path.resolve(__dirname, './webview/src/index.tsx')
+    paths.appSrc = path.resolve(__dirname, './webview/src')
+    paths.testsSetup = path.resolve(__dirname, './webview/src/setupTests.js')
+    paths.proxySetup = path.resolve(__dirname, './webview/src/setupProxy.js')
+    paths.appTypeDeclarations = path.resolve(__dirname, './webview/src/react-app-env.d.ts')
+    // paths.appBuild = path.resolve(__dirname, '../dist')
+		return paths
+	},
   devServer: (configFunction) => {
     return function(proxy, allowedHost) {
       // Create the default config by calling configFunction with the proxy/allowedHost parameters
@@ -9,7 +22,7 @@ module.exports = {
       config.setup = (app, server) => {
         app.get('/renderer.js', (req, res) => {
           res.setHeader('Content-Type', 'text/javascript')
-          const content = fs.readFileSync('./renderer.js', 'utf8')
+          const content = fs.readFileSync('./app/renderer.js', 'utf8')
           res.send(content)
         })
       }
--- webview/src/App.tsx
+++ webview/src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React22
         </a>
       </header>
     </div>
